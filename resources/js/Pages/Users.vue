12a1163c5714217f0628bdae5f83fdf4096dfc07
zachary tecson <zactec115@gmail.com>
2023-10-23 00:33:14 +0800

    merge conflict

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <h1 class="text-3xl">Users</h1>
 
 
@@ -19,26 +18,6 @@ export default {
         time: String,
     }
 }
-=======
-    <Layout>
-        <h1 class="text-3xl">Users</h1>
-
-
-        <div style="margin-top: 600px">
-            <p>The current time now is {{ time }}</p>
-        </div>
-        <Link class="text-blue-500" href="/users" preserve-scroll>Refresh</Link>
-    </Layout>
-</template>
-
-<script setup>
-import Layout from '../Shared/Layout.vue';
-import { Link } from '@inertiajs/vue3';
-
-defineProps({
-    time: String,
-})
->>>>>>> 662147fbcd80a529d4171f96b3961ddf64e094dd
 </script>
 
 
