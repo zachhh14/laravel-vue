99ad8e124b7e4e73ea1921f4e1b16bd7bc171791
zachary tecson <zactec115@gmail.com>
2023-10-22 18:05:48 +0800

    initialize vue.js

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Hello, {{ name }}</h1>
+    <span>In this crash course, we will use the following frameworks:</span>
+    <ul>
+        <li v-for="framework in frameworks">{{ framework }}</li>
+    </ul>
+
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/users">Users</a></li>
+            <li><a href="/settings">Settings</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array,
+    }
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Inertia\Inertia;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home',[
+        'name' => 'Zachary',
+        'frameworks' => ['Laravel','Inertia(not a framework)', 'vue']
+    ]);
+});
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
 });
