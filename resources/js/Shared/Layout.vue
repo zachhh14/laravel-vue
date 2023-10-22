5e55b1c640adebad1b75cb7517907acde3fc27a8
zachary tecson <zactec115@gmail.com>
2023-10-22 21:21:11 +0800

    shared data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,7 +1,12 @@
 <template>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
-            <h1 class="font-bold text-lg">My App</h1>
+            <div class="flex items-center">
+                <h1 class="font-bold text-lg">My App</h1>
+                <p class="text-sm ml-4">
+                    Welcome Back, {{ username}}
+                </p>
+            </div>
             <Nav />
         </header>
     </section>
@@ -15,6 +20,11 @@
 <script>
 import Nav from './Nav.vue'
 export default{
-    components:{ Nav}
+    components:{ Nav },
+    computed:{
+        username(){
+            return this.$page.props.auth.user.username;
+        }
+    }
 }
 </script>
